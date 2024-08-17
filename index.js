4650038319e25c6694083d5bd8dd7e68fae1619c
devSharma14 <devs140124@gmail.com>
2024-08-17 14:38:37 +0530

    updating index.js

--- index.js
+++ index.js
@@ -20,15 +20,11 @@ app.get('/', (req, res) => {
 app.use('/posts', postRoutes);
 app.use('/user', userRouter);
 
-const PORT = 8000;
 // MongoDB Connection
 const CONNECTION_URL = 'mongodb+srv://devs140124:24uRXg8pXEfPTRp9@cluster0.cy5ubvu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 
 mongoose.connect(CONNECTION_URL)
-  .then(() => {
-    // Start the server
-    app.listen(PORT, () => console.log(`Server is listening on port: ${PORT}`));
-  })
+  .then(() => console.log('MongoDB connected'))
   .catch((error) => console.log(error.message));
 
 // Export the app as a serverless function
