f0b9c6263163f12ed7950e95fa7c519d899ff5ed
Dev Sharma <148771436+devSharma14@users.noreply.github.com>
2025-07-30 12:06:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ mongoose.connect(CONNECTION_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 }).then(() => {
-  app.listen(PORT, () => {
+  app.listen(PORT, '0.0.0.0', () => {
     console.log(`🚀 Server running on port: ${PORT}`);
   });
 }).catch((error) => {
